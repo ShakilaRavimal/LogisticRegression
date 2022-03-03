6606b11a555804244cfae49def376075c424723d
Shakila <shakilaravimal@gmail.com>
2022-03-03 22:41:09 +0530

    fix "column_drop" function

--- LogisticRegression/LogisticRegression.cpp
+++ LogisticRegression/LogisticRegression.cpp
@@ -106,7 +106,7 @@ void column_drop(vector<int> drops, vector<vector<T>>& tdata)
     {
         if (k > 0)
         {
-            drops[k] = drops[k] - 1;
+            drops[k] = drops[k] - k;
         }
         for (int i = 0; i < tdata.size(); i++)
         {
